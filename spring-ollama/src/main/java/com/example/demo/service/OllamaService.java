1a2ad0710d78d9de4dad922c2d4eeb1cdd6756f8
tgts_root <tgts.root@wisseninfotech.com>
2025-06-23 16:07:14 +0530

    LLM model Change

--- spring-ollama/src/main/java/com/example/demo/service/OllamaService.java
+++ spring-ollama/src/main/java/com/example/demo/service/OllamaService.java
@@ -17,6 +17,18 @@ public class OllamaService {
 
 	public String chatWithOllama(String prompt) {
 		Map<String, Object> body = Map.of("model", "phi", "prompt", prompt, "stream", false);
+		System.err.println("calling PI");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+
+		ResponseEntity<Map> response = restTemplate.postForEntity(OLLAMA_URL, request, Map.class);
+		System.err.println(response);
+		return response.getBody().get("response").toString();
+	}
+
+	public String generateImageWithOllama(String prompt) {
+		Map<String, Object> body = Map.of("model", "", "prompt", prompt, "stream", false);
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
